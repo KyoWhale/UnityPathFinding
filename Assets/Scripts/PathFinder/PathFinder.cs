dbbd559dd3ed170754c14989ae04d0b118eb906a
KoWhale <kyosun97@gmail.com>
2023-08-03 14:28:07 +0900

    추가 : 여러 알고리즘을 적용할 수 있는 길찾기 추상 클래스

--- Assets/Scripts/PathFinder/PathFinder.cs
+++ Assets/Scripts/PathFinder/PathFinder.cs
@@ -13,37 +13,58 @@ public class Node : IComparable<Node>
     public Node previous;
     public int x, y;
     public int cost;
-    public Direction direction; // Direction from previous
+
+    public Node() { }
+
+    public Node(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Node(int x, int y, int cost, Node previous)
+    {
+        this.x = x;
+        this.y = y;
+        this.cost = cost;
+        this.previous = previous;
+    }
 
     public int CompareTo(Node other)
     {
         return cost - other.cost;
     }
+
+    public override bool Equals(object obj)
+    {
+        Node other = obj as Node;
+        if (other == null)
+        {
+            return false;
+        }
+        return other.x == x && other.y == y;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(x, y);
+    }
 }
 
-public abstract class PathFinder : MonoBehaviour
+public abstract class Pathfinder : MonoBehaviour
 {
-    public bool autoStart = true;
-
-    public Vector2Int start;
-    public Vector2Int end;
-    protected Node _start = new Node();
-    protected Node _current = new Node();
-    protected Node _end = new Node();
+    protected Node _startNode;
+    protected Node _endNode;
 
     protected MinHeap<Node> _openNodes = new MinHeap<Node>();
+    protected HashSet<Node> _visitedNodes = new HashSet<Node>();
 
-    public float waitingTime;
+    private float waitingTime = 0.1f;
     protected WaitForSeconds _wait;
 
     private void Start()
     {
         _wait = new WaitForSeconds(waitingTime);
-
-        if (autoStart)
-        {
-            StartPathFinding();
-        }
     }
 
     public void StartPathFinding()
@@ -54,41 +75,56 @@ public abstract class PathFinder : MonoBehaviour
 
     private void SetNodes()
     {
-        _start.x = start.x;
-        _start.y = start.y;
-        _end.x = end.x;
-        _end.y = end.y;
+        if (Map.instance == null || Map.instance.startCell == null || Map.instance.destinationCell == null)
+        {
+            Debug.LogError("No start or destination node");
+            _startNode = new Node(0, 0);
+            _endNode = new Node(Map.instance.width - 1, Map.instance.height - 1);
+            return;
+        }
+        _startNode = new Node(Map.instance.startCell.x, Map.instance.startCell.y);
+        _endNode = new Node(Map.instance.destinationCell.x, Map.instance.destinationCell.y);
+        Debug.Log(GetType().ToString() + _startNode.x + ", " + _startNode.y);
     }
 
     private IEnumerator FindPath()
     {
-        _openNodes.Push(_start);
+        _openNodes.Push(_startNode);
         while(_openNodes.Count > 0)
         {
-            _current = _openNodes.Pop();
-            if (_current.x == _end.x && _current.y == _end.y)
+            Node currentNode = _openNodes.Pop();
+            if (_visitedNodes.Contains(currentNode))
             {
-                _end = _current;
-                break;
+                continue;
             }
+            _visitedNodes.Add(currentNode);
 
-            var newNodes = SearchFrom(_current);
+            var newNodes = SearchNeighbor(currentNode);
             foreach (var node in newNodes)
             {
+                if (node.x == _endNode.x && node.y == _endNode.y)
+                {
+                    IteratePath(node);
+                    yield break;
+                }
                 _openNodes.Push(node);
+                Map.instance.SetCellType(node.x, node.y, CellType.OpenList);
                 yield return _wait;
             }
 
+            Map.instance.SetCellType(currentNode.x, currentNode.y, CellType.Visited);
             yield return _wait;
-        }
-
-        if (_current != _end)
-        {
-            Debug.LogError("Can not reach to destination");
-            yield break;
         }
     }
 
-    protected abstract List<Node> SearchFrom(Node current);
-    protected abstract void CalculateCost(Node current);
+    protected abstract List<Node> SearchNeighbor(Node current);
+    private void IteratePath(Node endNode)
+    {
+        var current = endNode;
+        while (current != null)
+        {
+            Map.instance.SetCellType(current.x, current.y, CellType.Complete);
+            current = current.previous;
+        }
+    }
 }
